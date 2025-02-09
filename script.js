9301ab72b5f502288d5fb8bf91d3d49b99397491
schalchlincode <schalchlincode@gmail.com>
2025-02-09 00:21:56 -0600

    Add basic math functions (add, subtract, multiply, divide)

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(add(2, 6));
+console.log(subtract(10, 4));
+console.log(multiply(3, 5));
+console.log(divide(8, 2));
